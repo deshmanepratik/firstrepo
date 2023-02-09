86aab478ca45b9c3fd9a19694baf5ff52345d971
Pratik Deshmane <deshmane.pratik@gmail.com>
2023-02-09 16:25:59 +0800

    fixed typo errors

--- if linear.py
+++ if linear.py
@@ -5,8 +5,8 @@ Created on Fri May  1 00:41:01 2020
 @author: deshmanp
 """
 
-a = 3
-b = 9
+a = 11
+b = 10
 
 def prat(a):
     print ("a is greater : "+str(a))
@@ -17,4 +17,4 @@ def prat_2(b):
     return b*b
 
 greater = prat(a) if a>b else prat_2(b)
-print ("and square of greated value is : "+str(greater))
+print ("and square of greater value is : "+str(greater))
